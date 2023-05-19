97181bb89dad1d21378f936ac686227778e152a2
Mohd Fawaz Akhtar <69739644+mfakhtar@users.noreply.github.com>
2023-05-19 15:02:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
+variable "token" {}
 terraform {
+  cloud {
+  token = var.token
+  }
   required_providers {
     random = {
       source  = "hashicorp/random"
